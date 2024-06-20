95bef2657c25116806495013280028705350b7ed
rzeraat.tur <rzeraat.tur@gmail.com>
2024-06-21 00:12:13 +0300

    fix another test

--- src/test/java/com/pmd/app/service/UserDetailsServiceImplTest.java
+++ src/test/java/com/pmd/app/service/UserDetailsServiceImplTest.java
@@ -1,17 +1,20 @@
 package com.pmd.app.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.pmd.app.model.User;
 import com.pmd.app.repository.UserRepository;
 
+@SpringBootTest
 public class UserDetailsServiceImplTest {
-
   @InjectMocks
   private UserDetailsServiceImpl userDetailsService;
 
@@ -30,6 +33,17 @@ public class UserDetailsServiceImplTest {
 
     when(userRepository.findByUsername(user.getUsername())).thenReturn(user);
 
+  }
+
+  @Test
+  void loadUserByUsername_WhenUserDoesNotExist_ShouldThrowUsernameNotFoundException() {
+    // Arrange
+    String username = "testUser";
+
+    when(userRepository.findByUsername(username)).thenReturn(null);
+
+    // Act and Assert
+    assertThrows(UsernameNotFoundException.class, () -> userDetailsService.loadUserByUsername(username));
   }
 
 }
